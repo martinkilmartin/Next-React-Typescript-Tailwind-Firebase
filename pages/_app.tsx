85d3b5ee95a0cb42b5570ffcb44617af4759216c
sionnach <sionnach.solutions@gmail.com>
2021-05-11 22:31:23 -0400

    Adds skeleton for NextWebVitalsMetric

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
 
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  const body = JSON.stringify(metric)
+  const url = 'https://example.com/analytics'
+
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body)
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true })
+  }
 }
 
 export default MyApp

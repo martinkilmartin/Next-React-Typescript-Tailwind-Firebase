d31c80946735454f794e1c830974f9c67326b849
sionnach <sionnach.solutions@gmail.com>
2021-06-03 10:46:43 -0400

    Sets responsive widths for Container component

--- components/containers/Container.tsx
+++ components/containers/Container.tsx
@@ -8,6 +8,10 @@ type Props = {
 const Container = ({
   children,
   style = 'container max-w-full px-4 mx-auto',
-}: Props) => <div className={style}>{children}</div>
+}: Props) => (
+  <div className="container max-w-full px-4 mx-auto lg:max-w-screen-xl 2xl:max-w-screen-2xl">
+    {children}
+  </div>
+)
 
 export default Container

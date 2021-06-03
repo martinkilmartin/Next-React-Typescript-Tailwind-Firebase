76061a4198bde25c8821a721f3729a9e8f63f556
sionnach <sionnach.solutions@gmail.com>
2021-06-03 11:12:45 -0400

    Refactors Hero Speel component

--- components/ui/Heroes/Speel.tsx
+++ components/ui/Heroes/Speel.tsx
@@ -2,6 +2,8 @@ type Props = {
   speel: string | JSX.Element
 }
 
-const Speel = ({ speel }: Props) => <p className="prose lg:prose-xl">{speel}</p>
+const Speel = ({ speel }: Props) => (
+  <p className="mb-5 prose lg:prose-xl">{speel}</p>
+)
 
 export default Speel

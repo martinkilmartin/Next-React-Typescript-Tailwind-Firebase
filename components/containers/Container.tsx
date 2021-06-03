0c168a84d2f5cd995fab619fdcc153a969184852
sionnach <sionnach.solutions@gmail.com>
2021-06-03 11:20:07 -0400

    Hardcodes Container component style

--- components/containers/Container.tsx
+++ components/containers/Container.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from 'react'
 
 type Props = {
-  children?: ReactNode
-  style?: string
+  children: ReactNode
 }
 
-const Container = ({
-  children,
-  style = 'container max-w-full px-4 mx-auto lg:max-w-screen-xl 2xl:max-w-screen-2xl',
-}: Props) => <div className={style}>{children}</div>
+const Container = ({ children }: Props) => (
+  <div className="container max-w-full px-4 mx-auto lg:max-w-screen-xl 2xl:max-w-screen-2xl">
+    {children}
+  </div>
+)
 
 export default Container

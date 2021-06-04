2c0a630a81b9c32a93567c4f2454697661663b85
sionnach <sionnach.solutions@gmail.com>
2021-06-04 11:29:37 -0400

    Improves Header & Navigation responsive UI

--- components/landmarks/Header.tsx
+++ components/landmarks/Header.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-
-import Brand from '../ui/Brand'
-import MenuButton from '../ui/Buttons/MenuButton'
 import HeaderNavigation from './Navigation/HeaderNavigation'
 
 type Props = {
@@ -9,25 +5,9 @@ type Props = {
 }
 
 function Header({ brandName }: Props) {
-  const MOBILE_MENU_ID = 'mobile-menu'
-  const [open, setOpen] = useState(false)
   return (
     <header>
-      <div className="flex justify-between w-1 px-2 mx-2 mt-2 sm:hidden">
-        <Brand brandName={brandName} />
-        <MenuButton
-          open={open}
-          setOpen={setOpen}
-          ariaControl={MOBILE_MENU_ID}
-          sRMessage="Open main menu"
-        />
-      </div>
-      <div className="mb-2 text-base shadow-lg navbar bg-base">
-        <div className="hidden sm:block">
-          <Brand brandName={brandName} />
-        </div>
-        <HeaderNavigation menuId={MOBILE_MENU_ID} open={open} />
-      </div>
+      <HeaderNavigation brandName={brandName} />
     </header>
   )
 }
--- components/landmarks/Navigation/HeaderNavigation.tsx
+++ components/landmarks/Navigation/HeaderNavigation.tsx
@@ -1,13 +1,35 @@
+import { useState } from 'react'
+
+import Brand from '../../ui/Brand'
 import HeaderMenu from './HeaderMenu'
+import MenuButton from '../../ui/Buttons/MenuButton'
 
 type Props = {
-  menuId: string
-  open: boolean
+  brandName?: string
 }
 
-function HeaderNavigation({ menuId, open }: Props) {
+function HeaderNavigation({ brandName }: Props) {
+  const MOBILE_MENU_ID = 'mobile-menu'
+  const [open, setOpen] = useState(false)
   return (
-    <nav aria-label="Main" id={menuId}>
+    <nav
+      aria-label="Main"
+      id={MOBILE_MENU_ID}
+      className="container px-6 py-3 mx-auto md:flex"
+    >
+      <div className="flex items-center justify-between">
+        <div>
+          <Brand brandName={brandName} />
+        </div>
+        <div className="flex md:hidden">
+          <MenuButton
+            open={open}
+            setOpen={setOpen}
+            ariaControl={MOBILE_MENU_ID}
+            sRMessage="Open main menu"
+          />
+        </div>
+      </div>
       <HeaderMenu open={open} />
     </nav>
   )

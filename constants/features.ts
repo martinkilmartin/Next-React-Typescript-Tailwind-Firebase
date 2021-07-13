efe3a6c3e1107b00ce1f2fef1efd37d5adee9d34
sionnach <sionnach.solutions@gmail.com>
2021-07-13 13:51:22 -0400

    Adds images to features

--- constants/features.ts
+++ constants/features.ts
@@ -5,48 +5,92 @@ export const FEATURES: Feature[] = [
     name: 'Change of Programme',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat lacus, pretium in quam eget, dapibus porta sem. Suspendisse in erat interdum, venenatis urna id, elementum mi. Sed sapien risus, lacinia eu leo id, condimentum elementum neque. Duis consequat risus ac dui venenatis, vitae luctus orci fringilla. Pellentesque facilisis et sem ut fringilla. Pellentesque non dolor ut libero cursus consequat in et libero. Sed eu dignissim nisi. Morbi maximus feugiat mi, et efficitur nisi accumsan ullamcorper.',
-    icon: '',
+    img: {
+      src:
+        '/img/square/signpost-timoleague-clonakilty-kilbrittain-garrettstown-kinsale-cork-ireland-1600w.jpg',
+      alt: '',
+      width: 1600,
+      height: 1600,
+    },
   },
   {
     name: 'Change of Hotel',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat lacus, pretium in quam eget, dapibus porta sem. Suspendisse in erat interdum, venenatis urna id, elementum mi. Sed sapien risus, lacinia eu leo id, condimentum elementum neque. Duis consequat risus ac dui venenatis, vitae luctus orci fringilla. Pellentesque facilisis et sem ut fringilla. Pellentesque non dolor ut libero cursus consequat in et libero. Sed eu dignissim nisi. Morbi maximus feugiat mi, et efficitur nisi accumsan ullamcorper.',
-    icon: '',
+    img: {
+      src:
+        '/img/square/malcolm-mcgettigan-blackhead-lighthouse-near-whitehead-county-antrim-northern-ireland-1600w.jpg',
+      alt: '',
+      width: 1600,
+      height: 1600,
+    },
   },
   {
     name: 'Change of Dates',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat lacus, pretium in quam eget, dapibus porta sem. Suspendisse in erat interdum, venenatis urna id, elementum mi. Sed sapien risus, lacinia eu leo id, condimentum elementum neque. Duis consequat risus ac dui venenatis, vitae luctus orci fringilla. Pellentesque facilisis et sem ut fringilla. Pellentesque non dolor ut libero cursus consequat in et libero. Sed eu dignissim nisi. Morbi maximus feugiat mi, et efficitur nisi accumsan ullamcorper.',
-    icon: '',
+    img: {
+      src: '/img/square/vacation-calendar-reminder-1600w.jpg',
+      alt: '',
+      width: 1600,
+      height: 1600,
+    },
   },
   {
-    name: 'Change of Returns',
+    name: 'Cash Returns',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat lacus, pretium in quam eget, dapibus porta sem. Suspendisse in erat interdum, venenatis urna id, elementum mi. Sed sapien risus, lacinia eu leo id, condimentum elementum neque. Duis consequat risus ac dui venenatis, vitae luctus orci fringilla. Pellentesque facilisis et sem ut fringilla. Pellentesque non dolor ut libero cursus consequat in et libero. Sed eu dignissim nisi. Morbi maximus feugiat mi, et efficitur nisi accumsan ullamcorper.',
-    icon: '',
+    img: {
+      src: '/img/square/50-euro-notes-1600w.jpg',
+      alt: '',
+      width: 1600,
+      height: 1600,
+    },
   },
   {
     name: 'Ease of Access',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat lacus, pretium in quam eget, dapibus porta sem. Suspendisse in erat interdum, venenatis urna id, elementum mi. Sed sapien risus, lacinia eu leo id, condimentum elementum neque. Duis consequat risus ac dui venenatis, vitae luctus orci fringilla. Pellentesque facilisis et sem ut fringilla. Pellentesque non dolor ut libero cursus consequat in et libero. Sed eu dignissim nisi. Morbi maximus feugiat mi, et efficitur nisi accumsan ullamcorper.',
-    icon: '',
+    img: {
+      src:
+        '/img/square/silhouette-wheelchair-user-with-arms-raised-against-sunset-ocean-1600w.jpg',
+      alt: '',
+      width: 1600,
+      height: 1600,
+    },
   },
   {
     name: 'Promotion Code',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat lacus, pretium in quam eget, dapibus porta sem. Suspendisse in erat interdum, venenatis urna id, elementum mi. Sed sapien risus, lacinia eu leo id, condimentum elementum neque. Duis consequat risus ac dui venenatis, vitae luctus orci fringilla. Pellentesque facilisis et sem ut fringilla. Pellentesque non dolor ut libero cursus consequat in et libero. Sed eu dignissim nisi. Morbi maximus feugiat mi, et efficitur nisi accumsan ullamcorper.',
-    icon: '',
+    img: {
+      src: '/img/square/.jpg',
+      alt: '',
+      width: 1600,
+      height: 1600,
+    },
   },
   {
     name: 'Ease of Booking',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat lacus, pretium in quam eget, dapibus porta sem. Suspendisse in erat interdum, venenatis urna id, elementum mi. Sed sapien risus, lacinia eu leo id, condimentum elementum neque. Duis consequat risus ac dui venenatis, vitae luctus orci fringilla. Pellentesque facilisis et sem ut fringilla. Pellentesque non dolor ut libero cursus consequat in et libero. Sed eu dignissim nisi. Morbi maximus feugiat mi, et efficitur nisi accumsan ullamcorper.',
-    icon: '',
+    img: {
+      src: '/img/square/booking-button-keyboard-1600w.jpg',
+      alt: '',
+      width: 1600,
+      height: 1600,
+    },
   },
   {
     name: 'Free Cancellation',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat lacus, pretium in quam eget, dapibus porta sem. Suspendisse in erat interdum, venenatis urna id, elementum mi. Sed sapien risus, lacinia eu leo id, condimentum elementum neque. Duis consequat risus ac dui venenatis, vitae luctus orci fringilla. Pellentesque facilisis et sem ut fringilla. Pellentesque non dolor ut libero cursus consequat in et libero. Sed eu dignissim nisi. Morbi maximus feugiat mi, et efficitur nisi accumsan ullamcorper.',
-    icon: '',
+    img: {
+      src:
+        '/img/square/tourist-using-phone-credit-card-booking-ticket-online-1600w.jpg',
+      alt: '',
+      width: 1600,
+      height: 1600,
+    },
   },
 ]

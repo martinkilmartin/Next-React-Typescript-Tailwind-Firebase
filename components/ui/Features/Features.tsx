62acc622cd4383d1e38c0e8280d0c46207c9909e
sionnach <sionnach.solutions@gmail.com>
2021-07-13 16:45:45 -0400

    Adds props to Features

--- components/ui/Features/Features.tsx
+++ components/ui/Features/Features.tsx
@@ -2,18 +2,17 @@ import { Feature } from '../../../interfaces/index'
 import FeatureCard from '../Cards/FeatureCard'
 
 type Props = {
+  title: string
+  subTitle: string
+  speel?: string
   features: Feature[]
 }
-const Features = ({ features }: Props) => (
+const Features = ({ title, subTitle, speel, features }: Props) => (
   <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8 text-base-content">
     <div className="prose prose-xl text-center max-w-none">
-      <h2>eCommerce</h2>
-      <h3>A better way to get online</h3>
-      <p>
-        You.ie takes all the hassle and jargon out of launching and maintaining
-        your online business presence. Always fast, always up-to-date, You.ie
-        takes cares of your online presence so you don't have to.
-      </p>
+      <h2>{title}</h2>
+      <h3>{subTitle}</h3>
+      {speel && <p>{speel}</p>}
     </div>
     <div className="mt-10">
       <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
